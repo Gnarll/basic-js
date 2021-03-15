1963317df28b146b058ccce42fe682277913ded8
Vadim Fedorovich <72505527+Gnarll@users.noreply.github.com>
2021-03-16 00:05:56 +0300

    feat: solve dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members) == false || members.length == 0) return false;
+  let onlyPeople = [];
+  for(let item of members) {
+    if(item == null || typeof(item) != "string") continue;
+    while(item.indexOf(" ") != -1) {
+    item = item.replace(" ", ""); 
+    }
+    if( (item[0] >= "A" && item[0] <= "Z") || (item[0] >= "a" && item[0] <= "z")) {
+      
+      onlyPeople.push(item.toUpperCase());
+     }
+  }
+  if(onlyPeople.length == 0) return false;
+  onlyPeople.sort();
+  let letters = onlyPeople.map(item => item[0]);
+  return letters.join("");
 };
